87dd88b2edae3b952950a220e00ed01d2c6495fa
Julian Garzon <bryangarzon22@gmail.com>
2020-06-06 12:29:42 -0500

    Firebase requests with VueResource

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import VueRouter from "vue-router";
+import VueResource from "vue-resource";
 
 import { routes } from "./routes";
 import store from "./store/store";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+Vue.http.options.root = "https://vuestocktrader-6172e.firebaseio.com/";
 
 Vue.use(VueRouter);
 const router = new VueRouter({ mode: "history", routes });
--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -5,9 +5,24 @@
     <v-spacer></v-spacer>
     <div class="font-weight-bold mr-2">Balance:</div>
     <div class="mr-10">{{ funds | currency }}</div>
-    <button class="font-weight-bold mr-10 red--text" @click="endDay">
+    <v-menu v-model="isDropdownOpen" :offset-y="true">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on" class="mr-10">
+          Save and Load
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="saveData">
+          <v-list-item-title>Save</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="loadData">
+          <v-list-item-title>Load</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-btn class="font-weight-bold mr-10 red--text" @click="endDay">
       END DAY
-    </button>
+    </v-btn>
   </v-app-bar>
 </template>
 
@@ -17,16 +32,32 @@ import { mapActions } from "vuex";
 export default {
   name: "Header",
   data: () => ({
-    drawer: true
+    drawer: true,
+    isDropdownOpen: false
   }),
   methods: {
-    ...mapActions(["randomizeStocks"]),
+    ...mapActions({
+      randomizeStocks: "randomizeStocks",
+      fetchData: "loadData"
+    }),
     changeDrawer() {
       this.drawer = !this.drawer;
       this.$emit("change-drawer", this.drawer);
     },
     endDay() {
       this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+
+      this.$http.put("data.json", data);
+    },
+    loadData() {
+      this.fetchData();
     }
   },
   computed: {
